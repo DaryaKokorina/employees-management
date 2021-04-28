3a613730fa1cd872f9e9e662e9a9737f72c0a249
Darya Kokorina <darya.kokorina15@gmail.com>
2021-04-28 16:25:57 +0300

    feat(emp): Create auth-form-container ui component

---
+++ libs/employee-app/ui-auth/src/lib/auth-form-container/auth-form-container.component.ts
@@ -0,0 +1,8 @@
+import { Component } from "@angular/core";
+
+@Component({
+  selector: 'emp-auth-form-container',
+  templateUrl: './auth-form-container.component.html',
+  styleUrls: ['./auth-form-container.component.css']
+})
+export class AuthFormContainerComponent {}
