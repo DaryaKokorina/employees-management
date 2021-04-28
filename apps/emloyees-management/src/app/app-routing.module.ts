48cf402564f7d18dbe5a53634c1e7ed3d1b6c749
Darya Kokorina <darya.kokorina15@gmail.com>
2021-04-28 17:01:06 +0300

    feat(app): Implement redirection to /login from starting the app

--- apps/emloyees-management/src/app/app-routing.module.ts
+++ apps/emloyees-management/src/app/app-routing.module.ts
@@ -1,8 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
+
 const routes: Routes = [
-  { path: '/', redirectTo: '/login', pathMatch: 'full' }
+  { path: '', redirectTo: '/login', pathMatch: 'full' },
+  {
+    path: 'login',
+    loadChildren: () => import('@dashasorg/employee-app/feature-employees-shell').then(m => m.EmployeeAppFeatureEmployeesShellModule)
+  }
 ];
 
 @NgModule({
--- libs/employee-app/feature-employees-shell/src/lib/employee-app-feature-employees-shell.module.ts
+++ libs/employee-app/feature-employees-shell/src/lib/employee-app-feature-employees-shell.module.ts
@@ -1,7 +1,20 @@
-import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
+import { Route, RouterModule } from '@angular/router';
+import { LoginComponent } from '@dashasorg/employee-app/feature-auth';
+
+
+export const empFeatureEmployeesShellRoutes: Route[] = [
+  {
+    path: '',
+    component: LoginComponent
+  }
+];
 
 @NgModule({
-  imports: [CommonModule],
+  imports: [
+    CommonModule,
+    RouterModule.forChild(empFeatureEmployeesShellRoutes)
+  ],
 })
 export class EmployeeAppFeatureEmployeesShellModule {}
