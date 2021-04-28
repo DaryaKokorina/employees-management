3576b1cd23619d100ee35c8bdc59f22642231aea
Darya Kokorina <darya.kokorina15@gmail.com>
2021-04-28 16:14:38 +0300

    feat(employee-app): Create ui-auth lib

--- jest.config.js
+++ jest.config.js
@@ -3,5 +3,6 @@ module.exports = {
     '<rootDir>/apps/emloyees-management',
     '<rootDir>/libs/employee-app/feature-employees-shell',
     '<rootDir>/libs/employee-app/feature-auth',
+    '<rootDir>/libs/employee-app/ui-auth',
   ],
 };
---
+++ libs/employee-app/ui-auth/README.md
@@ -0,0 +1,7 @@
+# employee-app-ui-auth
+
+This library was generated with [Nx](https://nx.dev).
+
+## Running unit tests
+
+Run `nx test employee-app-ui-auth` to execute the unit tests.
