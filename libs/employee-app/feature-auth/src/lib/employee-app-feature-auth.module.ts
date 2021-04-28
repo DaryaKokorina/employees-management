7ab7c023de2e4e4c0c8d8742d21201c870a88559
Darya Kokorina <darya.kokorina15@gmail.com>
2021-04-28 16:42:02 +0300

    feat(emp): Use uath-form container component in login component

--- libs/employee-app/feature-auth/src/lib/employee-app-feature-auth.module.ts
+++ libs/employee-app/feature-auth/src/lib/employee-app-feature-auth.module.ts
@@ -1,10 +1,12 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
+import { EmployeeAppUiAuthModule } from '@dashasorg/employee-app/ui-auth';
 import { LoginComponent } from './login/login.component';
 
 
+
 @NgModule({
-  imports: [CommonModule],
+  imports: [CommonModule, EmployeeAppUiAuthModule],
   declarations: [LoginComponent]
 })
 export class EmployeeAppFeatureAuthModule {}
