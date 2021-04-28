0cca7058dd7524f3347a55f4f55d78d1e4980c64
Darya Kokorina <darya.kokorina15@gmail.com>
2021-04-28 16:37:54 +0300

    feat(emp): Declare auth-form-container component and make fake layout

--- libs/employee-app/ui-auth/src/lib/employee-app-ui-auth.module.ts
+++ libs/employee-app/ui-auth/src/lib/employee-app-ui-auth.module.ts
@@ -1,7 +1,11 @@
-import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
+import { AuthFormContainerComponent } from './auth-form-container/auth-form-container.component';
+
 
 @NgModule({
   imports: [CommonModule],
+  declarations: [AuthFormContainerComponent],
+  exports: [AuthFormContainerComponent]
 })
 export class EmployeeAppUiAuthModule {}
