c5944934ab8dee005b86d04c74d5248c749ded92
Darya Kokorina <darya.kokorina15@gmail.com>
2021-04-29 18:38:12 +0300

    feat(emp): Add styles and change layout for login and auth-form-container component

--- libs/employee-app/feature-auth/src/lib/login/login.component.ts
+++ libs/employee-app/feature-auth/src/lib/login/login.component.ts
@@ -3,7 +3,8 @@ import { LoginData } from '@dashasorg/employee-app/models';
 
 @Component({
   selector: 'emp-login',
-  templateUrl: './login.component.html'
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css']
 })
 export class LoginComponent {
   login(data: LoginData) {
