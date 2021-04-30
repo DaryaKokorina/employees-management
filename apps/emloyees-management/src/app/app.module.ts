0503121e3e1f347fc91b0518ced046e2e1d3d12f
Darya Kokorina <darya.kokorina15@gmail.com>
2021-04-30 13:50:57 +0300

    feat(app): Add firebase packages and modules to the app

--- apps/emloyees-management/src/app/app.module.ts
+++ apps/emloyees-management/src/app/app.module.ts
@@ -1,12 +1,22 @@
 import { NgModule } from '@angular/core';
+import { AngularFireModule } from '@angular/fire';
+import { AngularFireAuthModule } from '@angular/fire/auth';
+import { AngularFirestoreModule } from '@angular/fire/firestore';
 import { BrowserModule } from '@angular/platform-browser';
+import { environment } from '../environments/environment';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    AngularFireModule.initializeApp(environment.firebase),
+    AngularFireAuthModule,
+    AngularFirestoreModule
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
