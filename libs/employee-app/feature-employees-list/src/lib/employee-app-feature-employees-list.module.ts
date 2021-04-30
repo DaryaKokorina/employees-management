1237357e4299273c84b6c890e7d69ca1bde35a21
Darya Kokorina <darya.kokorina15@gmail.com>
2021-04-30 15:06:32 +0300

    feat(emp): Use employees-list component into the employees-list-container component

--- libs/employee-app/feature-employees-list/src/lib/employee-app-feature-employees-list.module.ts
+++ libs/employee-app/feature-employees-list/src/lib/employee-app-feature-employees-list.module.ts
@@ -1,7 +1,8 @@
-import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
+import { EmployeeAppUiEmployeesListModule } from '@dashasorg/employee-app/ui-employees-list';
 
 @NgModule({
-  imports: [CommonModule],
+  imports: [CommonModule, EmployeeAppUiEmployeesListModule],
 })
 export class EmployeeAppFeatureEmployeesListModule {}
