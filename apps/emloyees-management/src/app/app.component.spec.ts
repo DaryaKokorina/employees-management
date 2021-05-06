9c94883e9569ef0667d5e12871145333b2bf9253
Darya Kokorina <darya.kokorina15@gmail.com>
2021-05-06 12:50:12 +0300

    test(app): Fix test for app component according to the changed logic

--- apps/emloyees-management/src/app/app.component.spec.ts
+++ apps/emloyees-management/src/app/app.component.spec.ts
@@ -1,9 +1,11 @@
 import { TestBed } from '@angular/core/testing';
+import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
 
 describe('AppComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
+      imports: [RouterTestingModule],
       declarations: [AppComponent],
     }).compileComponents();
   });
@@ -12,11 +14,5 @@ describe('AppComponent', () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
     expect(app).toBeTruthy();
-  });
-
-  it(`should have as title 'emloyees-management'`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app.title).toEqual('emloyees-management');
   });
 });
