b926defae46abe240399cf1c8baa76ef8b92026f
Darya Kokorina <darya.kokorina15@gmail.com>
2021-05-07 12:51:47 +0300

    feat(emp): Create error-message component an dstyles it

--- libs/employee-app/ui-error-message/src/lib/employee-app-ui-error-message.module.ts
+++ libs/employee-app/ui-error-message/src/lib/employee-app-ui-error-message.module.ts
@@ -1,7 +1,10 @@
-import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
+import { ErrorMessageComponent } from './error-message/error.message.component';
 
 @NgModule({
   imports: [CommonModule],
+  declarations: [ErrorMessageComponent],
+  exports: [ErrorMessageComponent]
 })
 export class EmployeeAppUiErrorMessageModule {}
